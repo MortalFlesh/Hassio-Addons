68640d1a85a11ec01bd6ed1f1a57e94855253e75
Petr Chromec <mortal_flesh@seznam.cz>
2022-10-06 10:11:43 +0200

    Read live stats from eaton controller

--- eaton-history/run.sh
+++ eaton-history/run.sh
@@ -3,15 +3,19 @@
 
 set -e
 
-bashio::log.info "Hello from docker!"
+bashio::log.info "Eaton addon is starting!"
 
 # https://github.com/home-assistant/addons/blob/master/dhcp_server/data/run.sh#L10-L13
-# Create main config
-#DEFAULT_LEASE=$(bashio::config 'default_lease')
-#DNS=$(bashio::config 'dns|join(", ")')
-#DOMAIN=$(bashio::config 'domain')
-#MAX_LEASE=$(bashio::config 'max_lease')
+EATON_HOST=$(bashio::config 'eaton_host')
+EATON_NAME=$(bashio::config 'eaton_name')
+EATON_PASSWORD=$(bashio::config 'eaton_password')
 
-bashio::log.info "Trying alpine.3.16-arm64!"
-ls -l /app/alpine.3.16-arm64
-./alpine.3.16-arm64/home-console home:web:run
+mkdir eaton-history
+
+bashio::log.info "Running home console web server!"
+./alpine.3.16-arm64/home-console home:web:run \
+    --host="$EATON_HOST"            \ # Host IP of the eaton controller. \
+    --name="$EATON_NAME"            \ # Name for eaton controller. [default: "admin"] \
+    --password="$EATON_PASSWORD"    \ # Password for eaton controller. \
+    # --cookies-path=""             \ # Path for a credentials file. [default: "./eaton-cookies.json"]
+    # --history-path[=HISTORY-PATH] \ # Path for a downloaded history directory. [default: "./eaton-history"]
